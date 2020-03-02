3d36eb84c827907b47d63f967943c3eb6e54177d
tian <729299276@qq.com>
2020-03-02 20:56:54 +0800

    +

--- CommService/src/main/java/com/mayikt/service/TestService.java
+++ CommService/src/main/java/com/mayikt/service/TestService.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class TestService {
     public void teste(){
-        int i = 8;
+        int i = 85;
         int m =2+5;
         System.out.println(m);
     }
